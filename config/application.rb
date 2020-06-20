3f12fd6f2bd9c9c7851ec9a2b0f03be4e0647a80
Ivan Sosnin <sosnin.spb@yandex.ru>
2020-06-20 23:34:23 +0300

    add config and console

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,13 @@
 class Application < Roda
+  plugin :environments
+
   route do |r|
     r.root do
       JSON status: :ok
     end
 
-    r.get "hello" do
-      JSON status: :ok
+    r.get "pagination" do
+      JSON page_size: Settings.pagination.page_size
     end
   end
 end
