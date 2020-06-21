9e082d67cd2dbd5eea8113c586db110dc5950bb0
Ivan Sosnin <sosnin.spb@yandex.ru>
2020-06-21 16:24:19 +0300

    add plugin - sinatra dsl similar routes

--- app/routes/ad_routes.rb
+++ app/routes/ad_routes.rb
@@ -3,32 +3,30 @@ class AdRoutes < Application
   prepend Validations
   prepend ApiErrors
 
-  route do |r|
-    r.get do
-      page = params[:page].presence || 1
-      ads = Ad.reverse_order(:updated_at)
-      ads = ads.paginate(page.to_i, Settings.pagination.page_size)
-      serializer = AdSerializer.new(ads.all, links: pagination_links(ads))
+  get do
+    page = params[:page].presence || 1
+    ads = Ad.reverse_order(:updated_at)
+    ads = ads.paginate(page.to_i, Settings.pagination.page_size)
+    serializer = AdSerializer.new(ads.all, links: pagination_links(ads))
 
+    JSON serializer.serializable_hash
+  end
+
+  post do
+    ad_params = validate_with!(AdParamsContract)
+
+    result = Ads::CreateService.call(
+      ad: ad_params[:ad],
+      user_id: params[:user_id]
+    )
+
+    if result.success?
+      serializer = AdSerializer.new(result.ad)
+      response.status = 201
       JSON serializer.serializable_hash
-    end
-
-    r.post do
-      ad_params = validate_with!(AdParamsContract)
-
-      result = Ads::CreateService.call(
-        ad: ad_params[:ad],
-        user_id: params[:user_id]
-      )
-
-      if result.success?
-        serializer = AdSerializer.new(result.ad)
-        response.status = 201
-        JSON serializer.serializable_hash
-      else
-        response.status = 422
-        error_response result.ad
-      end
+    else
+      response.status = 422
+      error_response result.ad
     end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -2,4 +2,5 @@ class Application < Roda
   plugin :environments
   plugin :all_verbs
   plugin :symbolized_params
+  plugin :class_level_routing
 end
